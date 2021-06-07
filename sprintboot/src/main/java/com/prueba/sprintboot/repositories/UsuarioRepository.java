a112630600315aba626d3d53c7bb25bc10f5d9df
Johan Zambrano <ejzambrano4@espe.edu.ec>
2021-06-07 14:36:22 -0500

    Api Rest Full

---
+++ sprintboot/src/main/java/com/prueba/sprintboot/repositories/UsuarioRepository.java
@@ -0,0 +1,14 @@
+package com.prueba.sprintboot.repositories;
+
+import java.util.ArrayList;
+
+import com.prueba.sprintboot.models.UsuarioModel;
+
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UsuarioRepository extends CrudRepository<UsuarioModel, Long> {
+    public abstract ArrayList<UsuarioModel> findByPrioridad(Integer prioridad);
+
+}
